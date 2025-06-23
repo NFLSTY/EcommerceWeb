2f42f3f0a6e1c2048b9ae5936982063c5d2d3b39
NFLSTY <naufalsatya10@gmail.com>
2025-06-23 19:43:25 +0700

    new controller

---
+++ app/Http/Controllers/adminController/ProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\AdminController;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $product = Product::all();
+        return view('admin.product', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $product = Product::findOrFail($id);
+        return view('admin.product-detail', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
